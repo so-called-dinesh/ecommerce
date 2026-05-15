e50af0e194cae73bc19b14fc14cab5616c5f0edd
So_called_dinesh <mrdineshh@outlook.com>
2026-05-15 19:51:14 +0530

    validated OrderRequest

--- src/main/java/com/dinesh/ecommerce/model/dto/OrderRequest.java
+++ src/main/java/com/dinesh/ecommerce/model/dto/OrderRequest.java
@@ -1,10 +1,22 @@
 package com.dinesh.ecommerce.model.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public record OrderRequest(
+        @NotBlank(message = "customer name required")
         String customerName,
+
+        @NotBlank(message = "email is required")
+        @Email(message = "email must be valid")
         String email,
+
+        @NotEmpty(message = "order must have atleast one item")
+        @Valid
         List<OrderItemRequest> items
 ) {}
 
